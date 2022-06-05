c20ae1f94308a89bb8c79d45986fb16e032ac311
gandor999 <geodorruales@gmail.com>
2022-06-05 21:40:06 +0800

    style: delete unused imports

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import './App.css';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects'
-import NavBar from './components/NavBar';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import { useTransition, animated, useSpring } from 'react-spring'
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import NavBar from "./components/NavBar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useTransition, animated } from "react-spring";
 
 function App({ preload }) {
+  const location = useLocation();
 
-  const location = useLocation()
-
-  const transitions = useTransition(location, {    //Got this from a codesandbox
-    from: { opacity: 0, transform: 'scale(0.9)' },
-    enter: { opacity: 1, transform: 'scale(1)'},
-    leave: { opacity: 0, transform: 'scale(0.9)' },
-  })
-
+  const transitions = useTransition(location, {
+    //Got this from a codesandbox
+    from: { opacity: 0, transform: "scale(0.9)" },
+    enter: { opacity: 1, transform: "scale(1)" },
+    leave: { opacity: 0, transform: "scale(0.9)" },
+  });
 
   return (
     <div>
       <NavBar />
-      {
-        transitions((style, item) => (
-          <animated.div className="routes" key={item.key} style={style}>
-            <Routes location={item}>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/personal-web-portfolio/" element={<Home />} />
-              <Route path="/personal-web-portfolio/aboutme" element={<About preload={preload} />} />
-              <Route path="/personal-web-portfolio/projects" element={<Projects preload={preload} />} />
-            </Routes>
-          </animated.div>
-        ))
-        
-      }
+      {transitions((style, item) => (
+        <animated.div className="routes" key={item.key} style={style}>
+          <Routes location={item}>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/personal-web-portfolio/" element={<Home />} />
+            <Route
+              path="/personal-web-portfolio/aboutme"
+              element={<About preload={preload} />}
+            />
+            <Route
+              path="/personal-web-portfolio/projects"
+              element={<Projects preload={preload} />}
+            />
+          </Routes>
+        </animated.div>
+      ))}
     </div>
-    
-  )
-        
-     
-     
-
+  );
 }
 
 export default App;
- 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,51 @@
-import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined, HomeOutlined, QuestionOutlined, AlertOutlined   } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-
+import { Menu } from "antd";
+import {
+  HomeOutlined,
+  QuestionOutlined,
+  AlertOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-
   return (
+    <div className="font-link">
+      <div className="menu fixed-top">
+        <Menu
+          mode="horizontal"
+          className="justify-content-center align-items-center"
+        >
+          <Menu.Item
+            key="home"
+            className="d-flex align-items-center nav-text"
+            icon={<HomeOutlined className="" />}
+          >
+            <Link to="/personal-web-portfolio/" className="pt-3">
+              <strong>Home</strong>
+            </Link>
+          </Menu.Item>
+          <Menu.Item
+            key="about"
+            id="about-text"
+            className="d-flex align-items-center nav-text"
+            icon={<QuestionOutlined className="" />}
+          >
+            <Link to="/personal-web-portfolio/aboutme" className="pt-3">
+              <strong>About Me</strong>
+            </Link>
+          </Menu.Item>
 
-      <div className="font-link">
-       <div className="menu fixed-top">
-         <Menu mode="horizontal" className="justify-content-center align-items-center">
-           <Menu.Item key="home" className="d-flex align-items-center nav-text"  icon={<HomeOutlined className="" />}>
-             <Link to="/personal-web-portfolio/" className="pt-3"><strong>Home</strong></Link>
-           </Menu.Item>
-           <Menu.Item key="about" id="about-text" className="d-flex align-items-center nav-text"  icon={<QuestionOutlined className=""/>}>
-             <Link to="/personal-web-portfolio/aboutme" className="pt-3"><strong>About Me</strong></Link>
-           </Menu.Item>
-           
-           <Menu.Item key="project" id="projects-text" className="d-flex align-items-center nav-text"  icon={<AlertOutlined className=""/>}>
-              <Link to="/personal-web-portfolio/projects" className="pt-3"><strong>Projects</strong></Link>
-           </Menu.Item>
-         </Menu>
-       </div>
+          <Menu.Item
+            key="project"
+            id="projects-text"
+            className="d-flex align-items-center nav-text"
+            icon={<AlertOutlined className="" />}
+          >
+            <Link to="/personal-web-portfolio/projects" className="pt-3">
+              <strong>Projects</strong>
+            </Link>
+          </Menu.Item>
+        </Menu>
       </div>
-      
-    
+    </div>
   );
 }
